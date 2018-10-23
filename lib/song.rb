7e6c6bc7830b305aeb482b3cf03f27aa1405588f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 14:49:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   @@artists = []
-  
+  @@count = 0
+
   attr_reader :name, :artist, :genre
-  
+
   def initialize(name, artist, genre)
+    @@count += 1
     @name = name
     @artist = artist
     @@artists << artist
